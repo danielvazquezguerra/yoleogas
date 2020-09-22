585cd3cdff9798185afdf33936ada9c51082e841
danielvazquezguerra <daniel.vazquezguerra@gmail.com>
2020-09-22 22:10:55 +0200

    lo logre con el header

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -5,9 +5,17 @@ const Wrapper = () => {
 
     return (
 
-        <div className="FotoTitulo container d-flex align-items-end">
+        <div className="FotoTitulo container d-flex align-items-end justify-content-end">
+
+            <div className="ImgBox d-flex justify-content-start align-items-end">
+
+            <img className="ChicaImg" src="/img/chica-color.png" alt=""/>
 
             <h1>Comparte tu lectura del contador con nosotros</h1>
+
+            </div>
+
+
 
         </div>
     )
